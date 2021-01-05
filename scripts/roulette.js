5635eeed33e2d403704c1005ad6279e81f4c567f
Ray <rafaelgame2012@gmail.com>
2021-01-04 22:43:53 -0600

    Solucionado primer ticket

--- scripts/roulette.js
+++ scripts/roulette.js
@@ -1,20 +1,15 @@
 'use strict'
+function añadirDatos(){
+    let array_datos = [0];
 
-function tomarDatos(){
-    var array_datos = [];
-    /**Ticket #1: Esto lo tengo que automatizar, usando solo un input y leyendo datos
-     * de uno en uno cada vez que presione un botón
-     */
-    var dato1 = document.getElementById('thing1-input').value;
-    var dato2 = document.getElementById('thing2-input').value;
-    var dato3 = document.getElementById('thing3-input').value;
-    var dato4 = document.getElementById('thing4-input').value;
-
-    array_datos[0] = dato1;
-    array_datos[1] = dato2;
-    array_datos[2] = dato3;
-    array_datos[3] = dato4;
-
+    for (let i = 0; i < array_datos.length; i++) {
+        for (let j = 0; j < 1; j++) {
+            array_datos[i]=document.getElementById('thing-input').value;
+            document.getElementById('thing-input').value = ''; 
+        }
+        
+    }
+    
 
     /**Ticket #2: Aquí tengo algo que arreglar con las etiquetas, ya que al momento
      * de meter las cosas dentro de otras cajas para que el HTML quede
@@ -22,14 +17,23 @@ function tomarDatos(){
      * pueden seleccionar elementos del body directamente porque estan
      * en otra caja
     */
-    var index;
-    for(index in array_datos){
-        var nuevoElemento = document.createElement('li');
-        var nuevoTexto = document.createTextNode(array_datos[index]);
-
+    var e = 0;
+    for(let i = 0; i < array_datos.length; i++){
+        
+        
+        var nuevoElemento = document.createElement(`li`);
+        nuevoElemento.className = 'datos';
+        var nuevoTexto = document.createTextNode(array_datos[i]);
         nuevoElemento.appendChild(nuevoTexto);
-
         var selectorUL = document.getElementById('listOfThings');
-        selectorUL.appendChild(nuevoElemento);
+        selectorUL.appendChild(nuevoElemento);    
+            
+        
     }
+    
+}
+
+function tomarDatos(){
+    var dato = document.getElementsByClassName('datos');
+    console.log(dato);
 }
